d87bcdaec4fb31d375b96caced6b0fc4c738be42
Jenna Grieshop <jengriesh@gmail.com>
2023-03-29 17:21:08 -0500

    end of day
    Still am not getting the same results as imagej

--- hdf5_extraction.m
+++ hdf5_extraction.m
@@ -10,30 +10,66 @@ clear all
 close all
 clc
 
-stack = [];
+stack = zeros(480,640,'uint16');
 
 %% load in the file
 
 % h5disp('2529f11b-6c60-4bd9-b580-f37d9665ca65.hdf5');
 
-v = VideoWriter('test.avi', 'Grayscale AVI');
-open(v)
+% v = VideoWriter('test.avi', 'Grayscale AVI');
+% open(v)
 
-for i = 1:209
+t = Tiff('testing.tif', 'w');
+
+tagstruct.ImageLength = 480;
+tagstruct.ImageWidth = 640;
+tagstruct.BitsPerSample = 16;
+tagstruct.SamplesPerPixel = 1;
+tagstruct.Compression = Tiff.Compression.None;
+tagstruct.PlanarConfiguration = Tiff.PlanarConfiguration.Chunky;
+tagstruct.Photometric = Tiff.Photometric.MinIsBlack; 
+
+setTag(t,tagstruct);
+
+for i = 0:209
     
     frame_name = ['/ImageFrame_1_1_1_', num2str(i)];
     
     frame_data = h5read('2529f11b-6c60-4bd9-b580-f37d9665ca65.hdf5', frame_name);
+    frame_data = im2uint16(frame_data);
 
     % extract and reformat the data
-    gray_frame = frame_data(2,:,:);
-    gray_frame = squeeze(gray_frame);
-%     imshow(gray_frame);
+    msb = frame_data(1,:,:);
+    msb = squeeze(msb);
+    msb = (msb - 8);
+    msb = msb * 256;
+    
+    lsb = frame_data(2,:,:);
+    lsb = squeeze(lsb);
+    
+    gray_frame = msb + lsb;
+    gray_frame = rot90(gray_frame);
+    imshow(gray_frame);
 
 
-    writeVideo(v, gray_frame);
+%     writeVideo(v, gray_frame);
 
-    stack(:,:,i) = gray_frame;
+%     stack(:,:,i+1) = gray_frame;
+%     write(t, gray_frame);
 end
 
-close(v)
+% stack = im2uint16(stack);
+
+
+
+
+
+close(t);
+
+
+
+% for x = 1:length(stack)
+%         imwrite(stack(:,:,:,x),'All_frames.tif', 'Compression', 'none','WriteMode', "append");
+% end
+
+% close(v)
