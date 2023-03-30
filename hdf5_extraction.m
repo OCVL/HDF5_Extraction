d26a6eb1503a5c41b6531793704b81f841c84291
Jenna Grieshop <jengriesh@gmail.com>
2023-03-30 10:43:05 -0500

    All videos saved from HDF5 file - matlab
    Saves all 6 videos from the session that is saved in the HDF5 file

--- hdf5_extraction.m
+++ hdf5_extraction.m
@@ -6,60 +6,86 @@
 % grayscale AVIs
 
 
-% clear all
-% close all
-% clc
+clear all
+close all
+clc
 
+% initialize the stack matrix
 stack = zeros(480,640,'uint16');
 
 %% load in the file
 
 % h5disp('2529f11b-6c60-4bd9-b580-f37d9665ca65.hdf5');
 
-t = Tiff('testing.tif', 'w');
-
-tagstruct.ImageLength = 480;
-tagstruct.ImageWidth = 640;
-tagstruct.BitsPerSample = 16;
-tagstruct.SamplesPerPixel = 1;
-tagstruct.Compression = Tiff.Compression.None;
-tagstruct.PlanarConfiguration = Tiff.PlanarConfiguration.Chunky;
-tagstruct.Photometric = Tiff.Photometric.MinIsBlack; 
-
-
-for i = 0:209
+% for loop to go through eyes
+for a = 0:1
     
-    frame_name = ['/ImageFrame_1_1_1_', num2str(i)];
+    if a == 0
+        eye = 'OD';
+    else
+        eye = 'OS';
+    end
     
-    frame_data = h5read('2529f11b-6c60-4bd9-b580-f37d9665ca65.hdf5', frame_name);
-    frame_data = uint16(frame_data);
-    
+    % for loop to go through video number
+    for b = 0:2
+        if b == 0
+            vid = 'vid_0';
+        elseif b == 1
+            vid = 'vid_1';
+        else
+            vid = 'vid_2';
+        end
+        
+        % initialize everything needed to write the tiff stack
+        file_name = [eye, '_', vid, '.tif'];
+        t = Tiff(file_name, 'w');
+        tagstruct.ImageLength = 480;
+        tagstruct.ImageWidth = 640;
+        tagstruct.BitsPerSample = 16;
+        tagstruct.SamplesPerPixel = 1;
+        tagstruct.Compression = Tiff.Compression.None;
+        tagstruct.PlanarConfiguration = Tiff.PlanarConfiguration.Chunky;
+        tagstruct.Photometric = Tiff.Photometric.MinIsBlack;
+        
+        % for loop to go through each frame
+        for c = 0:209
+              
+            frame_name = ['/ImageFrame_', num2str(a), '_1_', num2str(b), '_', num2str(c)];
 
-    % extract and reformat the data
-    msb = frame_data(1,:,:);
-    msb = squeeze(msb);
-    
-    msb = (msb - 8);
-    msb = msb * 256;
-    
-    lsb = frame_data(2,:,:);
-    lsb = squeeze(lsb);
+            % read in the frame data and convert to uint16
+            frame_data = h5read('2529f11b-6c60-4bd9-b580-f37d9665ca65.hdf5', frame_name);
+            frame_data = uint16(frame_data);
 
-    gray_frame = msb + lsb;
-    gray_frame = rot90(gray_frame, 3);
 
-    stack(:,:,i+1) = gray_frame;
+            % extract and reformat the data
+            msb = frame_data(1,:,:);
+            msb = squeeze(msb);
 
+            msb = (msb - 8);
+            msb = msb * 256;
+
+            lsb = frame_data(2,:,:);
+            lsb = squeeze(lsb);
+
+            gray_frame = msb + lsb;
+            gray_frame = rot90(gray_frame, -1);
+
+            % add frame to stack
+            stack(:,:,c+1) = gray_frame;
+
+        end
+        % write the tiff stack
+        for ii=1:size(stack,3)
+           setTag(t,tagstruct);
+           write(t,stack(:,:,ii));
+           writeDirectory(t);
+        end
+        close(t)
+    end
 end
 
 
 
-for ii=1:size(stack,3)
-   setTag(t,tagstruct);
-   write(t,stack(:,:,ii));
-   writeDirectory(t);
-end
-close(t)
 
 
 
