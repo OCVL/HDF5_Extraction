cd29cbc2d913f1c1dc4926310553bdcb703ade3b
Robert F Cooper <robert.cooper@marquette.edu>
2023-06-08 12:38:36 -0500

    Generalized a few aspects of the MATLAB conversion file.

--- hdf5_extraction.m
+++ hdf5_extraction.m
@@ -11,87 +11,100 @@ close all
 clc
 
 % initialize the stack matrix
-stack = zeros(480,640,'uint16');
 
-%% load in the file
+thisfolder = pwd;
+thisfolder = uigetdir(thisfolder, 'Select the folder containing the CLight hdf5 data.');
+    
+[filelist]=read_folder_contents(thisfolder, 'hdf5');
+
+%% load in the files
 
 % h5disp('fef748ff-4fb2-4bcf-958b-eef4bf765240.hdf5');
+for file=filelist'
 
-% for loop to go through eyes
-for a = 0:1
+    fPath = fullfile(thisfolder, file{1});
     
-    if a == 0
-        eye = 'OD';
-    else
-        eye = 'OS';
-    end
+    % for loop to go through eyes
+    for a = 0:1
     
-    % for loop to go through video number
-    for b = 0:2
-        if b == 0
-            vid = 'vid_0';
-        elseif b == 1
-            vid = 'vid_1';
+        stack = zeros(480,640,'uint16');
+        
+        if a == 0
+            eye = 'OD';
         else
-            vid = 'vid_2';
+            eye = 'OS';
         end
         
-        
-        % initialize everything needed to write the tiff stack
-        file_name = [eye, '_', vid, '.tif'];
-        t = Tiff(file_name, 'w');
-        tagstruct.ImageLength = 480;
-        tagstruct.ImageWidth = 640;
-        tagstruct.BitsPerSample = 16;
-        tagstruct.SamplesPerPixel = 1;
-        tagstruct.Compression = Tiff.Compression.None;
-        tagstruct.PlanarConfiguration = Tiff.PlanarConfiguration.Chunky;
-        tagstruct.Photometric = Tiff.Photometric.MinIsBlack;
-        
-        % for loop to go through each frame
-        % 7 second vid = 209 (210 frames)
-        % 10 second vid = 299 (300 frames)
-        for c = 0:299
-              
-            frame_name = ['/ImageFrame_', num2str(a), '_1_', num2str(b), '_', num2str(c)];
+        % for loop to go through video number
+        for b = 0:2
+            if b == 0
+                vid = 'vid_0';
+            elseif b == 1
+                vid = 'vid_1';
+            else
+                vid = 'vid_2';
+            end
+            
+            
+            % initialize everything needed to write the tiff stack
+            file_name = [eye, '_', vid, '.tif'];
+            t = Tiff(file_name, 'w');
+            tagstruct.ImageLength = 480;
+            tagstruct.ImageWidth = 640;
+            tagstruct.BitsPerSample = 16;
+            tagstruct.SamplesPerPixel = 1;
+            tagstruct.Compression = Tiff.Compression.None;
+            tagstruct.PlanarConfiguration = Tiff.PlanarConfiguration.Chunky;
+            tagstruct.Photometric = Tiff.Photometric.MinIsBlack;
+            
+    
+            meta_name = ['/ScanMetaData_', num2str(a), '_1_', num2str(b)];
+    
+            frm_metadata = h5read(fPath, meta_name);
 
-            % read in the frame data and convert to uint16
-            frame_data = h5read('0e272883-2721-4ddd-98d3-b3f032f1ba31.hdf5', frame_name);
-            frame_data = uint16(frame_data);
+            datcontents=cellstr(frm_metadata.Data');
+            valcontents=cellstr(frm_metadata.Value');
+            
+            countind = find(startsWith(datcontents,'FrameCount'));
 
-
-            % extract and reformat the data
-            msb = frame_data(1,:,:);
-            msb = squeeze(msb);
-
-            msb = (msb - 8);
-            msb = msb * 256;
-
-            lsb = frame_data(2,:,:);
-            lsb = squeeze(lsb);
-
-            gray_frame = msb + lsb;
-            gray_frame = rot90(gray_frame, -1);
-
-            % add frame to stack
-            stack(:,:,c+1) = gray_frame;
-
+            numfrms = str2double(valcontents{countind});
+    
+            % for loop to go through each frame
+            % 7 second vid = 209 (210 frames)
+            % 10 second vid = 299 (300 frames)
+            for c = 0:numfrms-1
+                c  
+                frame_name = ['/ImageFrame_', num2str(a), '_1_', num2str(b), '_', num2str(c)];
+    
+                % read in the frame data and convert to uint16
+                frame_data = h5read(fPath, frame_name);
+                frame_data = uint16(frame_data);
+    
+    
+                % extract and reformat the data
+                msb = frame_data(1,:,:);
+                msb = squeeze(msb);
+    
+                msb = (msb - 8);
+                msb = msb * 256;
+    
+                lsb = frame_data(2,:,:);
+                lsb = squeeze(lsb);
+    
+                gray_frame = msb + lsb;
+                gray_frame = rot90(gray_frame, -1);
+    
+                % add frame to stack
+                stack(:,:,c+1) = gray_frame;
+    
+            end
+            % write the tiff stack
+            for ii=1:size(stack,3)
+               setTag(t,tagstruct);
+               write(t,stack(:,:,ii));
+               writeDirectory(t);
+            end
+            close(t)
         end
-        % write the tiff stack
-        for ii=1:size(stack,3)
-           setTag(t,tagstruct);
-           write(t,stack(:,:,ii));
-           writeDirectory(t);
-        end
-        close(t)
     end
 end
-
-
-
-
-
-
-
-
-
